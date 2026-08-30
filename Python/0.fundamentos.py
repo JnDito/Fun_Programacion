25be88274a501d7809462ca4b230e3c44db6c1da
Juan Vasquez <juand.delcampo01@gmail.com>
2026-08-29 20:02:24 -0500

    Sintaxis, entrada, salida, comentarios en Python

--- Python/0.fundamentos.py
+++ Python/0.fundamentos.py
@@ -1,1 +1,31 @@
 # Fundamentos del Lenguaje Python
+
+# Numero Entero
+edad = 19
+
+# Cadena de texto
+nombre = "Juan"
+
+# Numero decimal
+precio = 19.99
+
+# Valor logico
+estado = False
+
+# Valor Caracter
+genero = 'M'
+
+# Entrada Dato
+nombre = input("Ingrese su nombre: ")
+
+# Salida Dato
+print(f"Hola, {nombre}")
+
+# Comentario 1 limea
+
+'''
+    Comentario en Bloque
+'''
+
+# Ejecutar Programa Python
+python fundamentos.py
