8808e87192d6a5d8e75eb40d0e51298c4edf4e5b
Juan Vasquez <juand.delcampo01@gmail.com>
2026-09-12 20:08:23 -0500

    Funciones retorno, sin retorno, anidados en CSharp

--- CSharp/Fundamentos/Funciones/Program.cs
+++ CSharp/Fundamentos/Funciones/Program.cs
@@ -6,3 +6,61 @@ static void saludar()
 }
 
 saludar();
+
+Console.WriteLine("Funciones sin retorno");
+Console.WriteLine("----------------------");
+Menu();
+static void Menu()
+{
+    Console.WriteLine("1. Registrar");
+    Console.WriteLine("2. Buscar");
+    Console.WriteLine("3. Salir");
+}
+
+Console.WriteLine("Funciones con retorno");
+Console.WriteLine("----------------------");
+int resultado = Productos(8, 7);
+Console.WriteLine($"{resultado}, Resultado * 2 = {resultado * 2}");
+static int Productos(int a, int b)
+{
+    return a * b;
+}
+
+Console.WriteLine("Funciones con varios argumentos");
+Console.WriteLine("----------------------");
+
+static double Promedio(double t1, double t2, double t3, double ep, double ef)
+{
+    return t1 * 0.1 + t2 * 0.1 + t3 * 0.1 + ep * 0.2 + ef * 0.5;
+}
+
+double prom = Promedio(20, 20, 20, 14, 12);
+Console.WriteLine($"Promedio obtenido es: {prom}");
+
+Console.WriteLine("Funciones con paremetros predeterminados");
+Console.WriteLine("----------------------");
+
+static void saludar_estudiante(string nombre, string curso="Fundamentos de Programación")
+{
+    Console.WriteLine($"Hola {nombre}, Bienvenido a {curso}");
+}
+
+saludar_estudiante("Juan");
+saludar_estudiante("Juan", "Base de Datos");
+
+Console.WriteLine("Funciones Anidadas");
+Console.WriteLine("----------------------");
+
+static double Proceso_Compra(double monto)
+{
+    double aplicar_igv(double valor)
+    {
+        return valor * 0.18;
+    }
+    double igv = aplicar_igv(monto);
+    double total = monto + igv;
+    return total;
+}
+
+double venta = Proceso_Compra(550);
+Console.WriteLine($"Total a pagar: S/. {venta}");
