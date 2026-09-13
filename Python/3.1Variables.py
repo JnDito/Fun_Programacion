ecc9360b9b224f6cbec04157b6ee2819d7201b61
Juan Vasquez <juand.delcampo01@gmail.com>
2026-09-13 08:55:47 -0500

    Variable Glabal y No Local en Python

--- Python/3.1Variables.py
+++ Python/3.1Variables.py
@@ -4,4 +4,28 @@ def saludar():
     print(f"Hola, {nombre}, Bienvenido")
 
 saludar()
-print(nombre)
+## print(nombre)
+## Variables Globales
+edad = 25
+
+def mostrar_edad():
+    global edad
+    print(f"Tu edad es: {edad}")
+
+mostrar_edad()
+print(type(edad))
+
+## Variable No Local
+
+def principal():
+    subtotal = 0
+
+    def calcularmonto():
+        nonlocal subtotal
+        subtotal += 100
+        print(f"El nuevo valor es: {subtotal}")
+
+    calcularmonto()
+    print(f"El nuevo valor en Principal es: {subtotal}")
+
+principal()
